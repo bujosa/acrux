76c17b6f16d27da9be108db2d617fa26637d2b3d
bujosa <davidbujosa@gmail.com>
2022-12-29 03:08:32 -0400

    feat: finish get address

--- projects/I-week/digital-signature/get-address.js
+++ projects/I-week/digital-signature/get-address.js
@@ -1,15 +1,11 @@
-const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 
 function getAddress(publicKey) {
-    // First step, you'll need to take the first byte off the public key. 
-    // The first byte indicates the format of the key, whether it is in the compressed format or not. 
-    // The publicKey will be a Uint8Array so you can use the slice method to slice off the first byte.
+    const subs = publicKey.slice(1);
 
-    const publicKey = publicKey.slice(1);
-    const hash = keccak256(publicKey);
-    const address = hash.slice(-20);
-    return address;
+    const hash = keccak256(subs);
+
+    return hash.slice(-20);
 }
 
 module.exports = getAddress;
