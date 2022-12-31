804ed518edb817a222d31759088a0cc66d0fa869
bujosa <davidbujosa@gmail.com>
2022-12-30 22:18:09 -0400

    feat(iw): add is valid in block chain

--- projects/I-week/block-chain-network/__tests__/block-chain.test.js
+++ projects/I-week/block-chain-network/__tests__/block-chain.test.js
@@ -1,6 +1,7 @@
 const Blockchain = require('../block-chain');
 const Block = require('../block');
 const assert = require('assert');
+const SHA256 = require("crypto-js/sha256");
 
 let blockchain;
 describe('Linking Blocks', function () {
@@ -42,4 +43,38 @@ describe('Linking Blocks', function () {
             });
         });
     });
+});
+
+describe('isValid', function() {
+  let blockchain;
+  beforeEach(() => {
+    blockchain = new Blockchain();
+    blockchain.addBlock(new Block("Dan"));
+    blockchain.addBlock(new Block("Peter"));
+    blockchain.addBlock(new Block("James"));
+  });
+  
+  it('should be considered valid', function() {
+    assert(blockchain.isValid());
+  });
+
+  describe('tampering with a previousHash', function() {
+    beforeEach(() => {
+      blockchain.chain[1].previousHash = SHA256("gibberish");
+    });
+
+    it('should not be considered valid', function() {
+      assert(!blockchain.isValid());
+    });
+  });
+  
+  describe('tampering with data', function() {
+    beforeEach(() => {
+      blockchain.chain[0].data = "Something Else";
+    });
+
+    it('should not be considered valid', function() {
+      assert(!blockchain.isValid());
+    });
+  });
 });
--- projects/I-week/block-chain-network/block-chain.js
+++ projects/I-week/block-chain-network/block-chain.js
@@ -11,6 +11,19 @@ class Blockchain {
         block.previousHash = this.chain[this.chain.length - 1].toHash();
         this.chain.push(block);
     }
+
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.previousHash.toString() !== previousBlock.toHash().toString()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
 }
 
 module.exports = Blockchain;
