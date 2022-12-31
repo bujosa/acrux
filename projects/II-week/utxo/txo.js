e85bf931ad7cfdb41546f1e348a18915989d8edb
bujosa <davidbujosa@gmail.com>
2022-12-31 04:56:46 -0400

    feat(iiw): finish first txo exercice

--- projects/II-week/utxo/txo.js
+++ projects/II-week/utxo/txo.js
@@ -1,9 +1,11 @@
 class TXO {
     constructor(owner, amount) {
-        
+        this.owner = owner;
+        this.amount = amount;
+        this.spent = false;
     }
     spend() {
-        
+        this.spent = true;
     }
 }
 
