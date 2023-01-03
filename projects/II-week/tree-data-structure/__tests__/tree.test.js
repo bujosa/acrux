0b43ee1c94bf96ca45919b88c631453f9f1a4070
bujosa <davidbujosa@gmail.com>
2023-01-02 21:14:51 -0400

    feat(iiw): add hasNode method in tree

--- projects/II-week/tree-data-structure/__tests__/tree.test.js
+++ projects/II-week/tree-data-structure/__tests__/tree.test.js
@@ -58,41 +58,73 @@ describe('tree', () => {
       });
     });
 
-    describe('after adding a greater node', () => {
+    describe('after adding 5', () => {
       before(() => {
-        tree.addNode(new Node(7));
+        tree.addNode(new Node(5));
       });
 
-      it('should have add a right to the root', () => {
-        assert(tree.root.right, 'did not find a right node on the root!');
-        assert.equal(tree.root.right.data, 7);
+      it('should have a root', () => {
+        assert(tree.root, 'did not find a root on the tree');
+        assert.equal(tree.root.data, 5);
       });
 
-      describe('after adding another lesser node', () => {
+      it('should find 5', () => {
+        assert(tree.hasNode(5), 'did not find a node with data 5');
+      });
+
+      it('should not find 4', () => {
+        assert.isNotTrue(tree.hasNode(4));
+      });
+
+      describe('after adding 3', () => {
         before(() => {
-          tree.addNode(new Node(6));
+          tree.addNode(new Node(3));
         });
 
-        it('should have add to the left node', () => {
-          assert(
-            tree.root.right.left,
-            'did not find a left on the right node on the root!'
-          );
-          assert.equal(tree.root.right.left.data, 6);
+        it('should have add a left to the root', () => {
+          assert(tree.root.left, 'did not find a left node on the root!');
+          assert.equal(tree.root.left.data, 3);
+        });
+
+        it('should find 3', () => {
+          assert(tree.hasNode(3), 'did not find a node with data 3');
+        });
+
+        it('should not find 4', () => {
+          assert.isNotTrue(tree.hasNode(4));
+        });
+
+        describe('after adding 4', () => {
+          before(() => {
+            tree.addNode(new Node(4));
+          });
+
+          it('should have add to the left node', () => {
+            assert(
+              tree.root.left.right,
+              'did not find a right on the left node on the root!'
+            );
+            assert.equal(tree.root.left.right.data, 4);
+          });
+
+          it('should find 4', () => {
+            assert(tree.hasNode(4), 'did not find a node with data 4');
+          });
         });
       });
 
-      describe('after adding another lesser node', () => {
+      describe('after adding 7', () => {
         before(() => {
-          tree.addNode(new Node(8));
+          tree.addNode(new Node(7));
         });
 
-        it('should have add to the left node', () => {
-          assert(
-            tree.root.right.right,
-            'did not find a second right node on the root!'
-          );
-          assert.equal(tree.root.right.right.data, 8);
+        it('should have add a right to the root', () => {
+          assert(tree.root.right, 'did not find a right node on the root!');
+          assert.equal(tree.root.right.data, 7);
+        });
+
+        it('should find 7', () => {
+          assert(tree.hasNode(7), 'did not find a node with data 7');
         });
       });
     });
--- projects/II-week/tree-data-structure/tree.js
+++ projects/II-week/tree-data-structure/tree.js
@@ -26,6 +26,21 @@ class Tree {
       }
     }
   }
+
+  hasNode(data) {
+    let current = this.root;
+    while (current) {
+      if (data === current.data) {
+        return true;
+      }
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
 }
 
 module.exports = {
