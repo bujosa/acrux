108c194145406d828361a6248e48f907acc7d740
bujosa <davidbujosa@gmail.com>
2023-01-16 01:07:57 -0400

    feat(iiiw): finish week 3, and add find ether

---
+++ projects/III-week/where-is-ether/find-ether.js
@@ -0,0 +1,29 @@
+const { providers } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+
+/**
+ * Given an ethereum address find all the addresses
+ * that were sent ether from that address
+ * @param {string} address - The hexidecimal address for the sender
+ * @async
+ * @returns {Array} all the addresses that receieved ether
+ */
+async function findEther(address) {
+  const blockNumber = await provider.getBlockNumber();
+  const recepients = [];
+
+  for (let i = 0; i < blockNumber; i++) {
+    const response = await provider.getBlockWithTransactions(i);
+    response.transactions.forEach((tx) => {
+      if (tx.from === address) {
+        recepients.push(tx.to);
+      }
+    });
+  }
+
+  return recepients;
+}
+
+module.exports = findEther;
