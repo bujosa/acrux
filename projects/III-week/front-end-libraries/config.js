151594a1602ac52738d2599db81d1509c66649e1
bujosa <davidbujosa@gmail.com>
2023-01-16 00:54:14 -0400

    fix(iiiw): update config

--- projects/III-week/front-end-libraries/config.js
+++ projects/III-week/front-end-libraries/config.js
@@ -1,17 +1,21 @@
-const ganache = require('ganache-cli');
+const { utils } = require('ethers');
+const Ganache = require('ganache-core');
+const PRIVATE_KEY =
+  '0xf2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d';
+const INITIAL_BALANCE = utils.parseEther('10');
 
-const ganacheProvider = ganache.provider({
-  accounts: [
-    {
-      secretKey: 'YOUR_SECRET_KEY_GOES_HERE',
-      balance: 1000000000000000000000,
-    },
-  ],
-});
+// create our test account from the private key, initialize it with 10 ether
+const accounts = [].concat([
+  {
+    balance: INITIAL_BALANCE.toHexString(),
+    secretKey: PRIVATE_KEY,
+  },
+]);
 
-const PRIVATE_KEY = 'YOUR_PRIVATE_KEY_GOES_HERE';
+const ganacheProvider = Ganache.provider({ accounts });
 
 module.exports = {
-  ganacheProvider,
+  INITIAL_BALANCE,
   PRIVATE_KEY,
+  ganacheProvider,
 };
